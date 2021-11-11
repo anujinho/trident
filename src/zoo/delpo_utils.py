bcb4a3cef3185f64a1130753c2f32a60894b9618
anujinho <anujrsingh1@gmail.com>
2021-11-11 23:15:55 +0100

    debugs

--- src/zoo/delpo_utils.py
+++ src/zoo/delpo_utils.py
@@ -93,7 +93,7 @@ def inner_adapt_delpo(task, reconst_loss, learner, n_ways, k_shots, q_shots, ada
     data, labels = task
     if args.dataset == 'miniimagenet':
         data, labels = data.to(device) / 255.0, labels.to(device)
-    elif args.dataset == 'omniglot':
+    elif (args.dataset == 'omniglot') or (args.dataset == 'cifarfs'):
         data, labels = data.to(device), labels.to(device)
     total = n_ways * (k_shots + q_shots)
     queries_index = np.zeros(total)
