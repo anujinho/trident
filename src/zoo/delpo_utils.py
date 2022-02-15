bc17c2e660eadb7881e1085b658656c45dbc7d44
anujinho <anujrsingh1@gmail.com>
2022-02-15 17:39:13 +0100

    added cub_test

--- src/zoo/delpo_utils.py
+++ src/zoo/delpo_utils.py
@@ -49,6 +49,17 @@ def setup(dataset, root, n_ways, k_shots, q_shots, order, inner_lr, device, down
         learner = CCVAE(in_channels=3, base_channels=32,
                         n_ways=n_ways, dataset='tiered', task_adapt=task_adapt, task_adapt_fn=task_adapt_fn, args=args)
 
+    elif (dataset == 'cub'):
+        image_trans = transforms.Compose([transforms.ToTensor(), transforms.Resize([84,84])])
+        train_tasks = gen_tasks(dataset, root, image_transforms=image_trans, download=download, mode='train',
+                                n_ways=n_ways, k_shots=k_shots, q_shots=q_shots)  # , num_tasks=50000)
+        valid_tasks = gen_tasks(dataset, root, image_transforms=image_trans, download=download, mode='validation',
+                                n_ways=n_ways, k_shots=k_shots, q_shots=q_shots)  # , num_tasks=10000)
+        test_tasks = gen_tasks(dataset, root, image_transforms=image_trans, download=download, mode='test',
+                               n_ways=n_ways, k_shots=k_shots, q_shots=q_shots, num_tasks=1000)
+        learner = CCVAE(in_channels=3, base_channels=32,
+                        n_ways=n_ways, dataset='miniimagenet', task_adapt=task_adapt, task_adapt_fn=task_adapt_fn, args=args)
+
     elif dataset == 'cifarfs':
         image_trans = transforms.Compose([transforms.ToTensor()])
         train_tasks = gen_tasks(dataset, root, image_transforms=image_trans, download=download, mode='train',
