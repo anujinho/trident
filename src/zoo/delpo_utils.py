22bfd1c37dd900cc36d0afc9eb77d09d92e6694b
anujinho <anujrsingh1@gmail.com>
2022-02-16 22:07:07 +0100

    added 5,1 cub

--- src/zoo/delpo_utils.py
+++ src/zoo/delpo_utils.py
@@ -56,7 +56,7 @@ def setup(dataset, root, n_ways, k_shots, q_shots, order, inner_lr, device, down
         valid_tasks = gen_tasks(dataset, root, image_transforms=image_trans, download=True, mode='validation',
                                 n_ways=n_ways, k_shots=k_shots, q_shots=q_shots)  # , num_tasks=10000)
         test_tasks = gen_tasks(dataset, root, image_transforms=image_trans, download=True, mode='test',
-                               n_ways=n_ways, k_shots=k_shots, q_shots=q_shots, num_tasks=1000)
+                               n_ways=n_ways, k_shots=k_shots, q_shots=q_shots, num_tasks=2000)
         learner = CCVAE(in_channels=3, base_channels=32,
                         n_ways=n_ways, dataset='miniimagenet', task_adapt=task_adapt, task_adapt_fn=task_adapt_fn, args=args)
 
